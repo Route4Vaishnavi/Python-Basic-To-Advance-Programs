e6262fa59697b6651167a3c3dd09c87372cd493d
Patil <v.g.patil@accenture.com>
2026-06-17 14:24:58 +0530

    Input and output

--- Hello.py
+++ Hello.py
@@ -1,1 +1,12 @@
 print('Hello, Vaishnavi')
+print('My name is Vaishnavi')
+print(2+2)
+
+#multiple inputs
+x , y = input("Enter 2 numbers:").split()
+print(x,y)
+
+#converting default string type to in and float
+x = int(input("Enter num :"))
+y = float(input("Evaluate 10/2 :"))
+print(x,y)
